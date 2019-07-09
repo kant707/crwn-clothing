4c8c721b58cc3201c66e8889ab9f367d631ed0ab
SHASHIKANT <kant707@gmail.com>
2019-07-09 16:50:45 +0530

    adding header component into global page space

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.componet';
+import Header from './components/header/header.component';
 
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
